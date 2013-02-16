63e14efc0a998244cf70d4b171241dd10ce4123c
Severin Schols <severin@schols.de>
2013-02-16 12:03:19 +0100

    Added Badges

--- main.js
+++ main.js
@@ -1,13 +1,50 @@
 var images;
 var imageCounter;
+var url1, src1;
 
 function start() {
 	images = new Object();
-	images["a"] = new Object();
-	images["a"]["url"] = "images/document.svg";
 
-	imageCounter = 1;
+	images.person = new Object();
+	images.person.url = "images/document.svg";
+	
+	images.a = new Object();
+	images.a.url = "images/document.svg";
+
+	images.badge01 = new Object();
+	images.badge01.url = "images/badges/" + $("#badge01").val();
+	images.badge02 = new Object();
+	images.badge02.url = "images/badges/" + $("#badge02").val();
+	images.badge03 = new Object();
+	images.badge03.url = "images/badges/" + $("#badge03").val();
+	images.badge04 = new Object();
+	images.badge04.url = "images/badges/" + $("#badge04").val();
+	images.badge05 = new Object();
+	images.badge05.url = "images/badges/" + $("#badge05").val();
+	images.badge06 = new Object();
+	images.badge06.url = "images/badges/" + $("#badge06").val();
+	images.badge07 = new Object();
+	images.badge07.url = "images/badges/" + $("#badge07").val();
+	images.badge08 = new Object();
+	images.badge08.url = "images/badges/" + $("#badge08").val();
+	images.badge09 = new Object();
+	images.badge09.url = "images/badges/" + $("#badge09").val();
+	images.badge10 = new Object();
+	images.badge10.url = "images/badges/" + $("#badge10").val();
+	
+	imageCounter = 12;
+	imageData("person", imageCallback);
 	imageData("a", imageCallback);
+	imageData("badge01", imageCallback);
+	imageData("badge02", imageCallback);
+	imageData("badge03", imageCallback);
+	imageData("badge04", imageCallback);
+	imageData("badge05", imageCallback);
+	imageData("badge06", imageCallback);
+	imageData("badge07", imageCallback);
+	imageData("badge08", imageCallback);
+	imageData("badge09", imageCallback);
+	imageData("badge10", imageCallback);
 }
 
 function imageCallback() {
@@ -65,20 +102,43 @@ function renderPDF() {
 	doc.addImage(images.a.data, 'JPEG', 100, 13, 30, 30);
 	
 	// Big Picture
-	doc.addImage(images.a.data, 'JPEG', 29, 50, 90, 90);
+	doc.addImage(images.person.data, 'JPEG', 29, 50, 90, 90);
 	
 	//Bagdes
-	doc.addImage(images.a.data, 'JPEG', 16, 150, 20, 20);
-	doc.addImage(images.a.data, 'JPEG', 40, 150, 20, 20);
-	doc.addImage(images.a.data, 'JPEG', 64, 150, 20, 20);
-	doc.addImage(images.a.data, 'JPEG', 88, 150, 20, 20);
-	doc.addImage(images.a.data, 'JPEG', 112, 150, 20, 20);
-	doc.addImage(images.a.data, 'JPEG', 16, 174, 20, 20);
-	doc.addImage(images.a.data, 'JPEG', 40, 174, 20, 20);
-	doc.addImage(images.a.data, 'JPEG', 64, 174, 20, 20);
-	doc.addImage(images.a.data, 'JPEG', 88, 174, 20, 20);
-	doc.addImage(images.a.data, 'JPEG', 112, 174, 20, 20);
+	doc.addImage(images.badge01.data, 'JPEG', 16, 150, 20, 20);
+	doc.addImage(images.badge02.data, 'JPEG', 40, 150, 20, 20);
+	doc.addImage(images.badge03.data, 'JPEG', 64, 150, 20, 20);
+	doc.addImage(images.badge04.data, 'JPEG', 88, 150, 20, 20);
+	doc.addImage(images.badge05.data, 'JPEG', 112, 150, 20, 20);
+	doc.addImage(images.badge06.data, 'JPEG', 16, 174, 20, 20);
+	doc.addImage(images.badge07.data, 'JPEG', 40, 174, 20, 20);
+	doc.addImage(images.badge08.data, 'JPEG', 64, 174, 20, 20);
+	doc.addImage(images.badge09.data, 'JPEG', 88, 174, 20, 20);
+	doc.addImage(images.badge10.data, 'JPEG', 112, 174, 20, 20);
 	
 	
 	doc.output("dataurlnewwindow", 'Test.pdf');
 }
+
+function getBadges(callback) {
+	$.getJSON('images/badges/list.json', function(data) {
+		callback(data);
+	});
+}
+
+$("window").ready(function() {
+	getBadges(function(lines) {
+		$.each(lines, function(idx,line) {
+			$("<option/>").val(line).text(line).appendTo("#badge01");
+			$("<option/>").val(line).text(line).appendTo("#badge02");
+			$("<option/>").val(line).text(line).appendTo("#badge03");
+			$("<option/>").val(line).text(line).appendTo("#badge04");
+			$("<option/>").val(line).text(line).appendTo("#badge05");
+			$("<option/>").val(line).text(line).appendTo("#badge06");
+			$("<option/>").val(line).text(line).appendTo("#badge07");
+			$("<option/>").val(line).text(line).appendTo("#badge08");
+			$("<option/>").val(line).text(line).appendTo("#badge09");
+			$("<option/>").val(line).text(line).appendTo("#badge10");
+		});
+	});
+});
